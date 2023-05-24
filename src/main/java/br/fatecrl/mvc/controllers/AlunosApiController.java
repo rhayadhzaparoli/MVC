27527d9d17f5f6140662a3915313b585450a4576
Mateus da Silva <rhayadh.zaparoli@outlook.com>
2023-05-24 09:45:18 -0300

    Updated

--- src/main/java/br/fatecrl/mvc/controllers/AlunosApiController.java
+++ src/main/java/br/fatecrl/mvc/controllers/AlunosApiController.java
@@ -1,35 +1,33 @@
-package br.fatecrl.mvc.controllers;
+package br.fatecrl.mvcdemo.controllers;
 
 import br.fatecrl.mvc.models.Alunos;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import br.fatecrl.mvcdemo.models.Aluno;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-@Controller
-@RequestMapping("/alunos")
-public class alunosController {
 
-    // simula banco de dados
+@RestController
+@RequestMapping("/api/alunos")
+public class AlunosApiController {
+
+
     private static final List<Alunos> alunos = new ArrayList<Alunos>();
 
-
-    public alunosController() {
+    public AlunosApiController() {
         alunos.add (new Alunos("111111", "Ana", 10, 5));
         alunos.add (new Alunos("222222", "Yasmin", 9, 9));
         alunos.add (new Alunos("333333", "Amanda", 8, 4));
         alunos.add (new Alunos("444444", "Rhayadh", 10, 8));
         alunos.add (new Alunos("555555", "Vitor", 8, 8));
         alunos.add (new Alunos("666666", "Michel", 7, 10));
-
-
     }
+
     @GetMapping
-    public String getAlunos(Model model){
-        model.addAttribute("alunos", alunos);
-        return "alunos";
-
+    public List<Alunos> getAlunos(){
+        return alunos;
     }
+
 }
---
+++ src/main/java/br/fatecrl/mvc/models/MvcDemoApplication.java
@@ -0,0 +1,13 @@
+package br.fatecrl.mvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(br.fatecrl.mvcdemo.MvcDemoApplication.class, args);
+    }
+
+}
